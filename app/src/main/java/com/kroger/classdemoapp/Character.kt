bf9bf3611b2e4ff15b8157493d3cecbe53a2737d
tiemonl <tiemon.liam@gmail.com>
2023-02-02 21:13:17 -0500

    add recycler view with dummy data

---
+++ app/src/main/java/com/kroger/classdemoapp/Character.kt
@@ -0,0 +1,11 @@
+package com.kroger.classdemoapp
+
+data class Character(
+    val name: String,
+    val age: Int,
+    val image: Int,
+    val universe: String,
+    val id: Int,
+    val relation: List<Int>
+)
+
